9fcb9316ddd42bb0525baed321116f7474283671
Kumar Samaksha <54833324+samaksha@users.noreply.github.com>
2026-09-02 09:52:04 +0530

    Add: minimum-stack - submission-29

---
+++ Data Structures & Algorithms/minimum-stack/submission-29.cpp
@@ -0,0 +1,38 @@
+using namespace std;
+
+class MinStack {
+public:
+    vector<long> stack;
+    long minimum = 0;
+
+    MinStack() {
+    }
+    
+    void push(int val) {
+        if(stack.size() == 0) {
+            minimum = val;
+            stack.push_back(0);
+        }
+        else {
+            stack.push_back(val - minimum);
+            minimum = min(minimum, (long)val);
+        }
+    }
+    
+    void pop() {
+        long val = stack.back();
+
+        if(val + minimum < minimum) minimum = minimum - val;
+
+        stack.pop_back();   
+    }
+    
+    int top() {
+        long val = stack.back();
+        return (val + minimum < minimum) ? minimum : val + minimum;
+    }
+    
+    int getMin() {
+        return minimum;
+    }
+};
